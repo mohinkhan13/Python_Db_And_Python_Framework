18d348cbd89bb5bce1384a97b19bb95d59ff8da5
mohinkhan13 <jatmohinkhan292061@gmail.com>
2024-09-20 11:01:10 +0530

    update 1

---
+++ project/product_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import ProductSubCat
+from django.db.models import Q
+
+def search_product(request):
+    query = request.GET.get('q')
+    if query:
+        products = ProductSubCat.objects.filter(
+            Q(product__product_name__icontains=query) |
+            Q(product_model__icontains=query) |
+            Q(product_price__icontains=query)
+        )
+    else:
+        products = ProductSubCat.objects.all()
+    return render(request, 'search.html', {'products': products})
